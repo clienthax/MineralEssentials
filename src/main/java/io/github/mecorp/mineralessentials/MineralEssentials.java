5b8835b73db0263f0b66ed8c957872cfec99a48d
untamemadman <samkemp55@gmail.com>
2014-08-25 16:33:39 +0100

    Started working on basics

---
+++ src/main/java/io/github/mecorp/mineralessentials/MineralEssentials.java
@@ -0,0 +1,16 @@
+package io.github.mecorp.mineralessentials;
+
+import cpw.mods.fml.common.Mod;
+import io.github.mecorp.mineralessentials.reference.Reference;
+
+/**
+ * Created by Sam on 8/25/2014.
+ */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class MineralEssentials
+{
+    @Mod.Instance(Reference.MOD_ID)
+    public static MineralEssentials instances;
+
+}
---
+++ src/main/java/io/github/mecorp/mineralessentials/reference/Reference.java
@@ -0,0 +1,11 @@
+package io.github.mecorp.mineralessentials.reference;
+
+/**
+ * Created by Sam on 8/25/2014.
+ */
+public class Reference
+{
+    public static final String MOD_ID = "MineralEssentials";
+    public static final String MOD_NAME = "Mineral Essentials";
+    public static final String VERSION = "1.7.10-2.0";
+}
