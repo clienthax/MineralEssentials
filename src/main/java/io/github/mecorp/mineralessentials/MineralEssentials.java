8026412df08c409d5fe2d881071078c44a5ebbe4
untamemadman <sam@untamemadman.pw>
2014-10-11 16:59:55 +0100

    Added Tin, Copper, Crystal, Iridium and Cobalt w/ textures

--- src/main/java/io/github/mecorp/mineralessentials/MineralEssentials.java
+++ src/main/java/io/github/mecorp/mineralessentials/MineralEssentials.java
@@ -5,11 +5,16 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import io.github.mecorp.mineralessentials.cobalt.Cobalt;
 import io.github.mecorp.mineralessentials.copper.Copper;
 import io.github.mecorp.mineralessentials.copper.blocks.CopperBlocks;
+import io.github.mecorp.mineralessentials.creative.Creative;
+import io.github.mecorp.mineralessentials.crystal.Crystal;
 import io.github.mecorp.mineralessentials.handler.ConfigHandler;
+import io.github.mecorp.mineralessentials.iridium.Iridium;
 import io.github.mecorp.mineralessentials.proxy.CommonProxy;
 import io.github.mecorp.mineralessentials.reference.Reference;
+import io.github.mecorp.mineralessentials.tin.Tin;
 
 /**
  * Created by Sam on 8/25/2014.
@@ -29,6 +34,11 @@ public class MineralEssentials
     {
         ConfigHandler.init(event.getSuggestedConfigurationFile());
         Copper.RegisterCopper();
+        Cobalt.RegisterCobalt();
+        Iridium.RegisterIridium();
+        Crystal.RegisterCrystal();
+        Tin.RegisterTin();
+        Creative.RegisterCreative();
     }
 
     @Mod.EventHandler
