59cc9d27a12dec463b8badea1f870b3023dcee82
untamemadman <sam@untamemadman.pw>
2014-11-08 00:03:50 +0000

    Version 2.0

--- src/main/java/io/github/mecorp/mineralessentials/reference/Reference.java
+++ src/main/java/io/github/mecorp/mineralessentials/reference/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 {
     public static final String MOD_ID = "MineralEssentials";
     public static final String MOD_NAME = "Mineral Essentials";
-    public static final String VERSION = "1.7.10-1.5";
+    public static final String VERSION = "1.7.10-2.0";
     public static final String ClientProxyClass = "io.github.mecorp.mineralessentials.proxy.ClientProxy";
     public static final String ServerProxyClass = "io.github.mecorp.mineralessentials.proxy.ServerProxy";
     public static final String GUIFactoryClass = "io.github.mecorp.mineralessentials.client.gui.GuiFactory";
