ab9993f1d8e1649b8a836936baf188f98f724547
Clienthax <clienthax@gmail.com>
2014-12-08 00:25:52 +0000

    Fix deprecated API use

--- src/main/java/io/github/mecorp/mineralessentials/proxy/ClientProxy.java
+++ src/main/java/io/github/mecorp/mineralessentials/proxy/ClientProxy.java
@@ -10,7 +10,7 @@ public class ClientProxy extends CommonProxy
     @Override
     public void initCapes()
     {
-        DevCapes.getInstance().registerConfig(Reference.DevCapes, Reference.MOD_ID);
+        DevCapes.getInstance().registerConfig(Reference.DevCapes);
         LogHelper.info("Capes Loaded");
     }
 }
